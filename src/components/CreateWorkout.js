9c0827563a5ae14e46b27fef82432ed8f6ed03b1
Scott Roberson <srkazan23@gmail.com>
2021-05-20 20:44:13 -0500

    added partial workout list to state

--- src/components/CreateWorkout.js
+++ src/components/CreateWorkout.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function CreateWorkout() {
+  const [lifts, setLifts] = useState([
+    { id: 'squat', title: 'Squat', selected: false },
+    { id: 'press', title: 'Press', selected: false },
+  ]);
+
+  const clicked = () => {};
+
   return (
     <div>
       <Link to='/logbook'>Cancel</Link>
       <h1>New Workout</h1>
       <input type='date' />
+      <div>
+        <h2>Select Your Lifts</h2>
+        <div>
+          {lifts.map((lift) => {
+            return (
+              <div onClick={clicked}>
+                <li>{lift.title}</li>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
